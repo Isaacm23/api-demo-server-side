0779f9958319b0ffa03ed859564ab7bbe6308840
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-05-11 01:00:14 -0700

    added prevent default to get the dubmit button to work

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-
-// var main = document.querySelector('#name');
-// var temp = document.querySelector('.temp');
-// var desc = document.querySelector('.desc');
-// var clouds = document.querySelector('.clouds');
-// var button= document.querySelector('.submit');
-
-// var queryURL = 
-
 $(document).ready(function () {
-//   $(".submit").click(function () {
-//     return getWeather();
-//   });
+  $(".submit").click(function () {
+    event.preventDefault();
+    return getWeather();
+  });
+});
 
-  // function getWeather() {
-  //   var val = $("#city").val();
-    
+function getWeather() {
+  var val = $("#city").val();
 
-    // if (val != "") {
-      $.ajax({
-        url:
-          "https://api.openweathermap.org/data/2.5/weather?q=austin&appid=dff0a319051040d9ba1c7ea38abb997d",
-        method: "GET",
-      }).then(function (response) {
-        console.log(response)
-        var cityName = results(response);
-        
-        $('#name').html(cityName)
-        
-      });
-    })
-//   }
-// });
+  
+    $.ajax({
+      url:
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        val +
+        "&appid=dff0a319051040d9ba1c7ea38abb997d",
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      var cityName = results(response);
+     
+      $("#name").html(cityName);
+    });
+  
+  }
 
-function results(response){
-return "<p> Name:"+response.name+"<p>"+
-"<p> Temperature:"+response.main.temp+"&deg;F<p>"+
-"<p> Humidity:"+response.main.humidity+"<p>"
 
-};    
+function results(response) {
+  return (
+    "<p> Name:" +
+    response.name +
+    "<p>" +
+    "<p> Temperature:" +
+    response.main.temp +
+    "&deg;F<p>" +
+    "<p> Humidity:" +
+    response.main.humidity +
+    "<p>"
+  );
+}
