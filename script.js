fa0a2daade0bca415df7143ccba4fe1364db95b5
Isaac Mckittrick <mckittri.isaa@gmail.com>
2020-05-15 01:19:22 -0700

    added local storage function

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ $(document).ready(function () {
   $(".submit").click(function () {
     event.preventDefault();
     return getWeather();
+    
   });
 });
 
 function getWeather() {
   var val = $("#city").val();
+  var cityString = "City";
+  localStorage.setItem(cityString, JSON.stringify(val))
+  var history= localStorage.getItem(val)
 
-  
-    $.ajax({
-      url:
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-        val +
-        "&appid=dff0a319051040d9ba1c7ea38abb997d&units=imperial",
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-     
-      var cityName = results(response);
-      
-      $("#name").html(cityName);
-      
-    });
-  
-  
-    
   $.ajax({
     url:
-      "https://api.openweathermap.org/data/2.5/forecast?q="+
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      val +
+      "&appid=dff0a319051040d9ba1c7ea38abb997d&units=imperial",
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+
+    var cityName = results(response);
+
+    $("#name").html(cityName);
+  });
+
+  $.ajax({
+    url:
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
       val +
       "&appid=dff0a319051040d9ba1c7ea38abb997d&units=imperial",
     method: "GET",
@@ -46,66 +46,131 @@ function getWeather() {
     $(".dayFive").html(five);
   });
 
-function results(response) {
-  return (
-    "<p>" +
-    response.name +
-    "<p>" +
-    "<p> Temperature:" +
-    response.main.temp +
-    "&deg;F<p>" +
-    "<p> With a high of:" +
-    response.main.temp_max + "<p>"+
-    "<p> With a Low of:" +
-    response.main.temp_min +
-    "<p>"
-  );
-}};
+  function results(response) {
+    return (
+      "<p>" +
+      response.name +
+      "<p>" +
+      "<p> Temperature: " +
+      response.main.temp +
+      "&deg;F<p>" +
+      "<p> High: " +
+      response.main.temp_max +
+      "<p>" +
+      "&deg;F<p> Low: " +
+      response.main.temp_min +
+      "&deg;F<p>" +
+      "<p>  <img src= http://openweathermap.org/img/wn/" +
+      response.weather[0].icon +
+      ".png>" +
+      "</p>" +
+      "<p>" +
+      response.weather[0].description +
+      "</p>"
+    );
+  }
+}
 
-function dayOne(data){
+function dayOne(data) {
   return (
-    "<p> Day "+ data.list[0].dt_txt +"</p>" +
-    "<p> High "+ data.list[0].main.temp_max + "&deg;F</p>"+
-    "<p> Low "+ data.list[0].main.temp_min + "&deg;F</p>" +
-   "<p>  <img src= http://openweathermap.org/img/wn/"+ data.list[0].weather[0].icon + ".png>" + "</p>"
+    "<p> Day " +
+    data.list[0].dt_txt +
+    "</p>" +
+    "<p> High " +
+    data.list[0].main.temp_max +
+    "&deg;F</p>" +
+    "<p> Low " +
+    data.list[0].main.temp_min +
+    "&deg;F</p>" +
+    "<p>  <img src= http://openweathermap.org/img/wn/" +
+    data.list[0].weather[0].icon +
+    ".png>" +
+    "</p>" +
+    "<p>" +
+    data.list[0].weather[0].description +
+    "</p>"
   );
-  
-};
-function dayTwo(data){
+}
+function dayTwo(data) {
   return (
-    "<p> Day "+ data.list[7].dt_txt +"</p>" +
-    "<p> High "+ data.list[7].main.temp_max + "&deg;F</p>"+
-    "<p> Low "+ data.list[7].main.temp_min + "&deg;F</p>" +
-    "<p>  <img src= http://openweathermap.org/img/wn/"+ data.list[7].weather[0].icon + ".png>" + "</p>"
-    
+    "<p> Day " +
+    data.list[7].dt_txt +
+    "</p>" +
+    "<p> High " +
+    data.list[7].main.temp_max +
+    "&deg;F</p>" +
+    "<p> Low " +
+    data.list[7].main.temp_min +
+    "&deg;F</p>" +
+    "<p>  <img src= http://openweathermap.org/img/wn/" +
+    data.list[7].weather[0].icon +
+    ".png>" +
+    "</p>" +
+    "<p>" +
+    data.list[0].weather[0].description +
+    "</p>"
   );
-  
-};
-function dayThree(data){
+}
+function dayThree(data) {
   return (
-    "<p> Day "+ data.list[15].dt_txt +"</p>" +
-    "<p> High "+ data.list[15].main.temp_max + "&deg;F</p>"+
-    "<p> Low "+ data.list[15].main.temp_min + "&deg;F</p>" +
-    "<p>  <img src= http://openweathermap.org/img/wn/"+ data.list[15].weather[0].icon + ".png>" + "</p>"
+    "<p> Day " +
+    data.list[15].dt_txt +
+    "</p>" +
+    "<p> High " +
+    data.list[15].main.temp_max +
+    "&deg;F</p>" +
+    "<p> Low " +
+    data.list[15].main.temp_min +
+    "&deg;F</p>" +
+    "<p>  <img src= http://openweathermap.org/img/wn/" +
+    data.list[15].weather[0].icon +
+    ".png>" +
+    "</p>" +
+    "<p>" +
+    data.list[0].weather[0].description +
+    "</p>"
   );
-  
-};
-function dayFour(data){
+}
+function dayFour(data) {
   return (
-    "<p> Day "+ data.list[23].dt_txt +"</p>" +
-    "<p> High "+ data.list[23].main.temp_max + "&deg;F</p>"+
-    "<p> Low "+ data.list[23].main.temp_min + "&deg;F</p>" +
-    "<p>  <img src= http://openweathermap.org/img/wn/"+ data.list[23].weather[0].icon + ".png>" + "</p>"
+    "<p> Day " +
+    data.list[23].dt_txt +
+    "</p>" +
+    "<p> High " +
+    data.list[23].main.temp_max +
+    "&deg;F</p>" +
+    "<p> Low " +
+    data.list[23].main.temp_min +
+    "&deg;F</p>" +
+    "<p>  <img src= http://openweathermap.org/img/wn/" +
+    data.list[23].weather[0].icon +
+    ".png>" +
+    "</p>" +
+    "<p>" +
+    data.list[0].weather[0].description +
+    "</p>"
   );
-  
-};
-function dayFive(data){
+}
+function dayFive(data) {
   return (
-    "<p> Day "+ data.list[31].dt_txt +"</p>" +
-    "<p> High "+ data.list[31].main.temp_max + "&deg;F</p>"+
-    "<p> Low "+ data.list[31].main.temp_min + "&deg;F</p>" +
-    
-    "<p>  <img src= http://openweathermap.org/img/wn/"+ data.list[31].weather[0].icon + ".png>" + "</p>"
+    "<p> Day " +
+    data.list[31].dt_txt +
+    "</p>" +
+    "<p> High " +
+    data.list[31].main.temp_max +
+    "&deg;F</p>" +
+    "<p> Low " +
+    data.list[31].main.temp_min +
+    "&deg;F</p>" +
+    "<p>  <img src= http://openweathermap.org/img/wn/" +
+    data.list[31].weather[0].icon +
+    ".png>" +
+    "</p>" +
+    "<p>" +
+    data.list[0].weather[0].description +
+    "</p>"
   );
-  
-};
+}
+
+
+ 
